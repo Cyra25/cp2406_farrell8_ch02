a34d62c15127502522c2f05c7a0ce804a651c6d6
Cyra25 <44994077+Cyra25@users.noreply.github.com>
2019-03-27 01:30:17 +0800

    display initial with period

---
+++ Exercises/Initials.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Initials {
+    public static void main(String[] args){
+        char firstInitial = 'C';
+        char secondInitial = 'S';
+        char thirdInitial = 'T';
+        char fourthInitial = 'T';
+        System.out.println("Hello, "+ firstInitial+"."+secondInitial+"."+thirdInitial+"."+fourthInitial);
+    }
+}
